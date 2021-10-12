aa719c3ec326676c2b0597aa83b3fcb0a773ae71
Nicolay <krjuchkovkolja@mail.ru>
2021-10-12 18:21:22 +0300

    new

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+require('./jquery.counterup.min');
+require('./jquery.nice-select.min');
+require('./jquery.waypoints.min');
+require('./lity.min');
+require('./main');
+require('./owl.carousel.min');
+require('./popper.min');
+require('./jquery-3.5.1.min');
+
+import Vue from "vue";
+
+// Пример компонента
+// Vue.component('example-component', require('./components/ExampleComponent').default);
+
+new Vue({
+    el: '#app',
+});
---
+++ webpack.default.mix.js
@@ -0,0 +1,10 @@
+// let mix = require('laravel-mix');
+// mix.postCss('src/css/app.css', 'dist/assets/css/app2.css')
+//         .sass('src/scss/app.scss', 'dist/assets/css');
+
+// Если не нужен сервер статики, закомментировать...
+// const browserSync = require("browser-sync").create();
+// browserSync.init({
+//     watch: true,
+//     server: "./dist"
+// });
