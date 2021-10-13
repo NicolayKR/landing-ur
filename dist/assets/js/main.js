630a4793ec411878a71ee072c2128091021dcbfa
Nicolay <krjuchkovkolja@mail.ru>
2021-10-13 14:59:25 +0300

    new

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -390,3 +390,11 @@ function swa2(){
     var nav = $('#fixed-navbar');
     nav.css('display', 'block');
 }
+$(document).ready(function(){
+    $("#open-nav-bar-menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
