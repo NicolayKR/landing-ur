e1f8082d356d4f80b325ca308ebedd10b0399bab
Ilya Kolesnikov <kolesnikov541@gmail.com>
2021-10-25 10:54:05 +0300

    slider vars

--- inc/slider.php
+++ inc/slider.php
@@ -1,67 +1,67 @@
 <?php
 
 $slider = [
-    1 => [
+    "{ad_id1}" => [
         'title' => 'Услуги юриста по алиментам',
         'subtitle' => 'БЕСПЛАТНАЯ ЮРИДИЧЕСКАЯ КОНСУЛЬТАЦИЯ',
         'img' => '/dist/assets/images/header/04_header-3.jpg'
     ],
-    2 => [
+    "{ad_id2}" => [
         'title' => [ 'Услуги юриста по разводу', 'Поможем решить любые вопросы по семейному праву!' ],
         'subtitle' => 'БЕСПЛАТНАЯ ЮРИДИЧЕСКАЯ КОНСУЛЬТАЦИЯ',
         'img' => '/dist/assets/images/header/04_header-3.jpg'
     ],
-    3 => [
+    "{ad_id3}" => [
         'title' => [ 'Услуги юриста по разделу имущества', 'Поможем решить любые вопросы в области раздела имущества!' ],
         'subtitle' => 'БЕСПЛАТНАЯ ЮРИДИЧЕСКАЯ КОНСУЛЬТАЦИЯ',
         'img' => '/dist/assets/images/header/04_header-3'
     ],
-    4 => [
+    "{ad_id4}" => [
         'title' => ['Услуги юриста по трудовым спорам', 'Взыщем зарплату и восстановим на работе быстро и выгодно!'],
         'subtitle' => 'БЕСПЛАТНАЯ ЮРИДИЧЕСКАЯ КОНСУЛЬТАЦИЯ',
         'img' => '/dist/assets/images/header/04_header-3'
     ],
-    5 => [
+    "{ad_id5}" => [
         'title' => 'Услуги юриста по семейным вопросам',
         'subtitle' => 'БЕСПЛАТНАЯ ЮРИДИЧЕСКАЯ КОНСУЛЬТАЦИЯ',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    6 => [
+    "{ad_id6}" => [
         'title' => ['Юристы по арбитражным делам', 'Ведение дел в арбитражных судах'],
         'subtitle' => '',
         'img' => '/dist/assets/images/header/04_header-3.jpg'
     ],
-    7 => [
+    "{ad_id7}" => [
         'title' => 'Поможем выбрать систему налогооблажения для бизнеса',
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    8 => [
+    "{ad_id8}" => [
         'title' => 'Все виды аудита для вашего бизнеса',
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    9 => [
+    "{ad_id9}" => [
         'title' => [ 'Восстановление отчетности', 'Восстановление учета любой сложности!' ],
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    10 => [
+    "{ad_id10}" => [
         'title' => ['Поможем сократить расходы Вашей компании', 'Услуги оптимизации налогов для бизнеса'],
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    11 => [
+    "{ad_id11}" => [
         'title' => ['Поможем вернуть налоги', 'Услуги по возврату налогов'],
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    12 => [
+    "{ad_id12}" => [
         'title' => ['Бухгалтерский аутсорсинг', '"Аутсорсинг бухгалтерии от экспертов "'],
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
     ],
-    13 => [
+    "{ad_id13}" => [
         'title' => [ 'Сдача отчетности. Быстро и без ошибок', 'Услуги по сдаче отчётности' ],
         'subtitle' => '',
         'img' => '/dist/assets/images/header/05_header-3.jpg'
